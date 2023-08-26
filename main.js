bdef99bb6cb56f42b5f8ef24b74df9b228272642
Knight <113319174+Original-Developer@users.noreply.github.com>
2023-08-26 18:19:32 +0200

    Refactor project structure

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ const { app, BrowserWindow } = require('electron');
 
 app.whenReady().then(function () {
     const win = new BrowserWindow({ width: 800, height: 600 });
-    win.loadFile('index.html')
+    win.loadFile('./pages/index.html')
 });
